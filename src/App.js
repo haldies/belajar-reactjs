37faad01fae8d3a94e1b291a8bb53db5095042ef
haldies <haldiespasya@gmail.com>
2023-06-13 19:30:56 +0700

    menambahkan fitur navbar "

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar/NavBar';
 
 function App() {
   return (
     <div>
       <header>
-        <nav>TESTING</nav>
+      <Navbar/>
       </header>
       <section>
         <h1>hello belajar react js day 1</h1>
